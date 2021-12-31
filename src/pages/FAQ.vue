a432f36e2f1fabbf6fb7fd35951686bcd50d82a4
Scholtz <ludko2@gmail.com>
2021-12-31 20:34:44 +0100

    faq update

--- src/pages/FAQ.vue
+++ src/pages/FAQ.vue
@@ -14,27 +14,7 @@
       <AccordionTab :header="$t('faq.q3')">
         <p>{{ $t("faq.a3") }}</p>
       </AccordionTab>
-      <AccordionTab :header="$t('faq.q4')">
-        <ol>
-          <li>{{ $t("faq.a4_1") }}</li>
-          <li>{{ $t("faq.a4_2") }}</li>
-          <li>{{ $t("faq.a4_3") }}</li>
-          <li>{{ $t("faq.a4_4") }}</li>
-        </ol>
-      </AccordionTab>
-      <AccordionTab :header="$t('faq.q5')">
-        <ol>
-          <li>{{ $t("faq.a5_1") }}</li>
-          <li>{{ $t("faq.a5_2") }}</li>
-          <li>{{ $t("faq.a5_3") }}</li>
-          <li>
-            {{ $t("faq.a5_4") }}
-          </li>
-          <li>
-            {{ $t("faq.a5_5") }}
-          </li>
-        </ol>
-      </AccordionTab>
+
       <AccordionTab :header="$t('faq.q6')">
         <p>
           {{ $t("faq.a6") }}
@@ -54,18 +34,16 @@
         </ol>
       </AccordionTab>
       <AccordionTab :header="$t('faq.q8')">
-        <ol>
-          <li>
-            {{ $t("faq.a8_1") }}
-          </li>
-          <li>{{ $t("faq.a8_2") }}</li>
-          <li>
-            {{ $t("faq.a8_3") }}
-          </li>
-          <li>
-            {{ $t("faq.a8_4") }}
-          </li>
-        </ol>
+        <p>
+          {{ $t("faq.a8_1") }}
+        </p>
+        <p>{{ $t("faq.a8_2") }}</p>
+        <p>
+          {{ $t("faq.a8_3") }}
+        </p>
+        <p>
+          {{ $t("faq.a8_4") }}
+        </p>
       </AccordionTab>
     </Accordion>
   </PublicLayout>
